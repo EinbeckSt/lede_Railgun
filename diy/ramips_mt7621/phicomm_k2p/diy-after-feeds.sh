7700143af0c925d22dd85680298fb19ab54cc8e5
EinbeckSt <120243632+EinbeckSt@users.noreply.github.com>
2023-01-31 19:26:25 +0800

    diy/mt7621: fix script path

--- diy/ramips_mt7621/phicomm_k2p/diy-after-feeds.sh
+++ diy/ramips_mt7621/phicomm_k2p/diy-after-feeds.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Modify Default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.99.2/g' package/base-files/files/bin/config_generate
 
 # Modify default hostname
-sed -i 's/ImmortalWrt/Railgun/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/PADAVAN-K2P/g' package/base-files/files/bin/config_generate
 
 # Enable 802.11 k/v/r
 sed -i 's/RRMEnable=0/RRMEnable=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
